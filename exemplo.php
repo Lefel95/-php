814a55b00256f18fa02e3cffb9332edcee448752
Lefel <lefel95@outlook.com>
2016-07-24 00:36:10 -0300

    Added based files and first print with var in PHP

---
+++ exemplo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+        $n = 20;
+        $no = "Felipe";
+        echo $no." tem ".$n." anos! </br>"; //ou
+        echo "$no tem $n anos!";
+    ?>
+</div>
+</body>
+</html>
